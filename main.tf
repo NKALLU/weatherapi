08d242affd1b811a29c28c49453c0ddfa0b42f93
Allu <n.m.subramanian@accenture.com>
2021-06-14 19:52:46 +0800

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.63.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "Australia East"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "nkbinarythistlewa"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "sathyamani/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
